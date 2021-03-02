231025682be811004471bc5b5c33e7b4bd45335b
H388kk <78017274+H388kk@users.noreply.github.com>
2021-03-02 14:29:05 +0200

    Pievienoju js

---
+++ static/js/script.js
@@ -0,0 +1,9 @@
+//alert("Hello")
+alert("Ralfs")
+console.log("Viss stradā!")
+
+let mainigaisvards = "Māja"
+let mainigaisskaitlis = 9
+
+console.log(mainigaisvards)
+console.log(mainigaisskaitlis)
